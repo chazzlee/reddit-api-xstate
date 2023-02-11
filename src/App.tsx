4c8d02233545c5caca30adf4609372e3714a2d38
chazzlee <l.chazz00@gmail.com>
2023-02-10 20:08:53 -0500

    add basic ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,39 @@
+import { useMachine } from "@xstate/react";
 import "./App.css";
+import { redditMachince } from "./redditMachine";
+
+const subreddits = ["frontend", "reactjs", "vuejs", "laravel"];
 
 function App() {
-  return <div className="App"></div>;
+  const [current, send] = useMachine(redditMachince);
+  const { subreddit, posts } = current.context;
+
+  return (
+    <div className="App">
+      <header>
+        <select
+          onChange={(e) => {
+            send({ type: "SELECT", name: e.target.value });
+          }}
+        >
+          {subreddits.map((subreddit) => (
+            <option key={subreddit}>{subreddit}</option>
+          ))}
+        </select>
+      </header>
+      <section>
+        <h1>{current.matches("idle") ? "Select a subreddit" : subreddit}</h1>
+        {current.matches({ selected: "loading" }) && <div>Loading...</div>}
+        {current.matches({ selected: "loaded" }) && (
+          <ul>
+            {posts?.map((post) => (
+              <li key={post.title}>{post.title}</li>
+            ))}
+          </ul>
+        )}
+      </section>
+    </div>
+  );
 }
 
 export default App;
